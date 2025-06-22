e1aacbc05539df18509702cb3b29b647dfb44be1
isfaaghyth <isfaaghyth@gmail.com>
2025-06-22 16:56:43 +0700

    feat: improve howto and blog components

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,15 +6,10 @@ const blogCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    thumbnail: z.string(),
     publishDate: z.string().transform(str => new Date(str)),
     author: z.string().default('Kepul'),
-    category: z.string(),
-    tags: z.array(z.string()),
+    category: z.string().default("Berita"),
   }),
 });
 
